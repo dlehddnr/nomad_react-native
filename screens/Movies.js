6c14f837a75dc7092950d148c5223b8ab92b3030
mac <dy95032@gmail.com>
2019-05-22 18:13:32 +0900

    Container-Presenter Begin

--- screens/Movies.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Text, TouchableOpacity } from "react-native";
-export default ({ navigation }) => (
-  <>
-    <TouchableOpacity onPress={() => navigation.navigate("Detail")}>
-      <Text>Go To Detail</Text>
-    </TouchableOpacity>
-  </>
-);
